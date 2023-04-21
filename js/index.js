452026798fc24f3a86141906f2e91b6fd15b1a96
abirangers <abiayyasi12@gmail.com>
2023-04-21 17:27:59 +0700

    initialized commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+const btn = document.querySelector('button');
+btn.addEventListener("click", function () {
+    navigasiBar = document.querySelector("nav");
+    navigasiBar.classList.toggle("aktif");
+});
+
+
