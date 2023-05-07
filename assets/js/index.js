d1fa4105d5fad09609300e52b552eb0f15d325fd
abirangers <abiayyasi12@gmail.com>
2023-05-07 21:03:20 +0700

    ubah font arab

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,29 +10,5 @@ function arrowBtn() {
     window.addEventListener("scroll", function () {
         arrow.classList.toggle("scrolled", window.scrollY > 500);
     });
-
-        // arrow.addEventListener('mousedown', mousedown);
-
-        // function mousedown(){
-        //     arrow.addEventListener('mousemove', mousemove);
-        //     arrow.addEventListener('mouseup', mouseup);
-            
-        //     function mousemove(e) {
-
-        //         let x = e.clientX - 100 + 'px';
-        //         let y = e.clientY - 100 + 'px';
-        //         this.style.left = x;
-        //         this.style.top = y;
-        //     }
-
-
-        
-        //     function mouseup() {
-        //         arrow.removeEventListener('mousemove', mousemove);
-        //     }
-
-        //  }
-
-
 }
 arrowBtn();
