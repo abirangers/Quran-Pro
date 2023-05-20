b0e09c4b8f8d0716ccf94ed9f528c7bce156e6c5
abirangers <abiayyasi12@gmail.com>
2023-05-20 22:00:13 +0700

    menambahkan fitur tafsir

--- assets/js/index.js
+++ assets/js/index.js
@@ -101,9 +101,10 @@ export function isiSurat() {
                 if(e.target === deskSurah) {
                     deskSurah.classList.remove('flex');
                     document.body.style.overflow = 'visible';
+                    navbarHeader.classList.remove('not-fixed');
                 }
             });
-    
+
             // isi surat
             const surat = response.ayat;
             let isiSurat = '';
@@ -112,6 +113,8 @@ export function isiSurat() {
                 isiSurat += surah(s);
             });
             containerSurat.innerHTML = isiSurat;
+
+            
 
             let mediaShare = document.querySelectorAll('.media-share');
             mediaShare.forEach(m => {
@@ -137,7 +140,43 @@ export function isiSurat() {
       }
       
     getSurat();
-    
+
+    function getTafsir() {
+        fetch(`https://equran.id/api/v2/tafsir/${nomorSurat}`)
+            .then(response => response.json())
+            .then(response => {
+                const ayatTafsir = document.querySelectorAll('.tafsiran');
+                ayatTafsir.forEach(a => {
+                    a.addEventListener('click', function() {
+                        // tafsir surat
+                        const navbarHeader = document.querySelector('.navbar');
+                        navbarHeader.classList.add('not-fixed');
+                        const tafsirSurah = document.querySelector('.tafsir-surah');
+                        tafsirSurah.classList.add('taft');
+                        document.body.style.overflow = 'hidden';  
+                        const dataVerse = a.getAttribute('data-verse');
+                        let dataTaf = response.data.tafsir;
+                        let data = response.data;
+                        let isiTafsir = '';
+                        dataTaf.forEach((taf, index) => {
+                            if(dataVerse == taf.ayat) {
+                                console.log(data);
+                                isiTafsir += tafsirOfSurah(taf, data);
+                            }
+                        });
+                        tafsirSurah.innerHTML = isiTafsir;
+                        window.addEventListener('click', function(e) {
+                            if(e.target === tafsirSurah) {
+                                tafsirSurah.classList.remove('taft');
+                                document.body.style.overflow = 'visible';
+                                navbarHeader.classList.remove('not-fixed');
+                            }
+                        });
+                    });
+                });
+            });
+    }
+    getTafsir();
 }
 
 function headerSurah(response) {
@@ -168,15 +207,25 @@ function audioSurah(response) {
 
 function surah(s) {
     return `
-    <div class="number-surah">
+    <div class="number-surah" data-verse="${s.nomor}">
         <div class="number">${s.nomor}</div>
         <div class="share">
             <svg class="media-share" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" class="fill-primary-700 dark:fill-primary-400" height="22" width="22" xmlns="http://www.w3.org/2000/svg"><title>Bagikan</title><path d="M752 664c-28.5 0-54.8 10-75.4 26.7L469.4 540.8a160.68 160.68 0 0 0 0-57.6l207.2-149.9C697.2 350 723.5 360 752 360c66.2 0 120-53.8 120-120s-53.8-120-120-120-120 53.8-120 120c0 11.6 1.6 22.7 4.7 33.3L439.9 415.8C410.7 377.1 364.3 352 312 352c-88.4 0-160 71.6-160 160s71.6 160 160 160c52.3 0 98.7-25.1 127.9-63.8l196.8 142.5c-3.1 10.6-4.7 21.8-4.7 33.3 0 66.2 53.8 120 120 120s120-53.8 120-120-53.8-120-120-120zm0-476c28.7 0 52 23.3 52 52s-23.3 52-52 52-52-23.3-52-52 23.3-52 52-52zM312 600c-48.5 0-88-39.5-88-88s39.5-88 88-88 88 39.5 88 88-39.5 88-88 88zm440 236c-28.7 0-52-23.3-52-52s23.3-52 52-52 52 23.3 52 52-23.3 52-52 52z"></path></svg>
-            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" class="fill-primary-700 dark:fill-primary-400" height="24" width="24" xmlns="http://www.w3.org/2000/svg"><title>Lihat Tafsir</title><path d="M928 161H699.2c-49.1 0-97.1 14.1-138.4 40.7L512 233l-48.8-31.3A255.2 255.2 0 0 0 324.8 161H96c-17.7 0-32 14.3-32 32v568c0 17.7 14.3 32 32 32h228.8c49.1 0 97.1 14.1 138.4 40.7l44.4 28.6c1.3.8 2.8 1.3 4.3 1.3s3-.4 4.3-1.3l44.4-28.6C602 807.1 650.1 793 699.2 793H928c17.7 0 32-14.3 32-32V193c0-17.7-14.3-32-32-32zM324.8 721H136V233h188.8c35.4 0 69.8 10.1 99.5 29.2l48.8 31.3 6.9 4.5v462c-47.6-25.6-100.8-39-155.2-39zm563.2 0H699.2c-54.4 0-107.6 13.4-155.2 39V298l6.9-4.5 48.8-31.3c29.7-19.1 64.1-29.2 99.5-29.2H888v488zM396.9 361H211.1c-3.9 0-7.1 3.4-7.1 7.5v45c0 4.1 3.2 7.5 7.1 7.5h185.7c3.9 0 7.1-3.4 7.1-7.5v-45c.1-4.1-3.1-7.5-7-7.5zm223.1 7.5v45c0 4.1 3.2 7.5 7.1 7.5h185.7c3.9 0 7.1-3.4 7.1-7.5v-45c0-4.1-3.2-7.5-7.1-7.5H627.1c-3.9 0-7.1 3.4-7.1 7.5zM396.9 501H211.1c-3.9 0-7.1 3.4-7.1 7.5v45c0 4.1 3.2 7.5 7.1 7.5h185.7c3.9 0 7.1-3.4 7.1-7.5v-45c.1-4.1-3.1-7.5-7-7.5zm416 0H627.1c-3.9 0-7.1 3.4-7.1 7.5v45c0 4.1 3.2 7.5 7.1 7.5h185.7c3.9 0 7.1-3.4 7.1-7.5v-45c.1-4.1-3.1-7.5-7-7.5z"></path></svg>
+            <svg class="tafsiran" data-verse="${s.nomor}" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" class="fill-primary-700 dark:fill-primary-400" height="24" width="24" xmlns="http://www.w3.org/2000/svg"><title>Lihat Tafsir</title><path d="M928 161H699.2c-49.1 0-97.1 14.1-138.4 40.7L512 233l-48.8-31.3A255.2 255.2 0 0 0 324.8 161H96c-17.7 0-32 14.3-32 32v568c0 17.7 14.3 32 32 32h228.8c49.1 0 97.1 14.1 138.4 40.7l44.4 28.6c1.3.8 2.8 1.3 4.3 1.3s3-.4 4.3-1.3l44.4-28.6C602 807.1 650.1 793 699.2 793H928c17.7 0 32-14.3 32-32V193c0-17.7-14.3-32-32-32zM324.8 721H136V233h188.8c35.4 0 69.8 10.1 99.5 29.2l48.8 31.3 6.9 4.5v462c-47.6-25.6-100.8-39-155.2-39zm563.2 0H699.2c-54.4 0-107.6 13.4-155.2 39V298l6.9-4.5 48.8-31.3c29.7-19.1 64.1-29.2 99.5-29.2H888v488zM396.9 361H211.1c-3.9 0-7.1 3.4-7.1 7.5v45c0 4.1 3.2 7.5 7.1 7.5h185.7c3.9 0 7.1-3.4 7.1-7.5v-45c.1-4.1-3.1-7.5-7-7.5zm223.1 7.5v45c0 4.1 3.2 7.5 7.1 7.5h185.7c3.9 0 7.1-3.4 7.1-7.5v-45c0-4.1-3.2-7.5-7.1-7.5H627.1c-3.9 0-7.1 3.4-7.1 7.5zM396.9 501H211.1c-3.9 0-7.1 3.4-7.1 7.5v45c0 4.1 3.2 7.5 7.1 7.5h185.7c3.9 0 7.1-3.4 7.1-7.5v-45c.1-4.1-3.1-7.5-7-7.5zm416 0H627.1c-3.9 0-7.1 3.4-7.1 7.5v45c0 4.1 3.2 7.5 7.1 7.5h185.7c3.9 0 7.1-3.4 7.1-7.5v-45c.1-4.1-3.1-7.5-7-7.5z"></path></svg>
         </div>
     </div>
     <p class="arabic">${s.ar}</p>
     <p class="teks">${s.tr}</p>
     <p class="translate">${s.idn}</p>`
+}
+
+function tafsirOfSurah(taf, data) {
+    return `
+    <div class="content-tafsir-surah">
+            <h3>tafsir ayat ${taf.ayat} surah ${data.namaLatin}</h3>
+            <div class="body-tafsir">
+                <p>${taf.teks}</p>
+            </div>
+        </div>`
 }
 
