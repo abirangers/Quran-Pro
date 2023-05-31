14e6aa09a79ac1e41c4f523a0b645dbe1188f171
abirangers <abiayyasi12@gmail.com>
2023-05-31 21:49:55 +0700

    memperbaiki bagikan surah

--- assets/js/index.js
+++ assets/js/index.js
@@ -68,7 +68,7 @@ export function isiSurat() {
   const nomorSurat = getURL("nomorsurat");
 
   function getSurat() {
-    fetch(`https://equran.id/api/surat/${nomorSurat}`)
+    fetch(`https://equran.id/api/surat/${nomorSurat}#${nomorSurat}`)
       .then((response) => response.json())
       .then((response) => {
         componentSurat(response);
@@ -82,12 +82,20 @@ export function isiSurat() {
           isiSurat += surah(s);
         });
         containerSurat.innerHTML = isiSurat;
-
+        const numberSurat = document.querySelectorAll('.number-surah');
+        idOfSurah(surat, numberSurat);
+        
         let mediaShare = document.querySelectorAll(".media-share");
         shareButton(mediaShare, response);
       });
-  }
-  getSurat();
+    }
+    getSurat();
+    
+    function idOfSurah(surat, numberSurat) {
+      numberSurat.forEach(surat => {
+        const idSurah = surat.getAttribute('id');
+      });
+    }
   function componentSurat(response) {
     // judul surat
     const contSurat = document.querySelector(".header-surah");
@@ -124,11 +132,16 @@ export function isiSurat() {
     mediaShare.forEach((m) => {
       m.addEventListener("click", function () {
         if (navigator.share) {
+          const nomorSurat = response.nomor; // Mengambil nomor surat dari response
+          const anchor = m.parentNode.parentNode.getAttribute('id'); // Mengambil nomor ayat dari atribut id parent node mediaShare
+          const currentURL = window.location.href; // Mendapatkan URL halaman saat ini
+          const url = `${currentURL}?nomorsurat=${nomorSurat}#${anchor}`;
+
           navigator
             .share({
               title: `${response.nama_latin}`,
               text: `${response.arti}`,
-              url: `https://facebook.com/`,
+              url: url, // Menggunakan URL yang sudah dibentuk
             })
             .then(function () {
               console.log("Berbagi berhasil.");
@@ -218,7 +231,7 @@ function audioSurah(response) {
 
 function surah(s) {
   return `
-    <div class="number-surah" data-verse="${s.nomor}">
+    <div id="${s.nomor}" class="number-surah" data-verse="${s.nomor}">
         <div class="number">${s.nomor}</div>
         <div class="share">
             <svg class="media-share" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" class="fill-primary-700 dark:fill-primary-400" height="22" width="22" xmlns="http://www.w3.org/2000/svg"><title>Bagikan</title><path d="M752 664c-28.5 0-54.8 10-75.4 26.7L469.4 540.8a160.68 160.68 0 0 0 0-57.6l207.2-149.9C697.2 350 723.5 360 752 360c66.2 0 120-53.8 120-120s-53.8-120-120-120-120 53.8-120 120c0 11.6 1.6 22.7 4.7 33.3L439.9 415.8C410.7 377.1 364.3 352 312 352c-88.4 0-160 71.6-160 160s71.6 160 160 160c52.3 0 98.7-25.1 127.9-63.8l196.8 142.5c-3.1 10.6-4.7 21.8-4.7 33.3 0 66.2 53.8 120 120 120s120-53.8 120-120-53.8-120-120-120zm0-476c28.7 0 52 23.3 52 52s-23.3 52-52 52-52-23.3-52-52 23.3-52 52-52zM312 600c-48.5 0-88-39.5-88-88s39.5-88 88-88 88 39.5 88 88-39.5 88-88 88zm440 236c-28.7 0-52-23.3-52-52s23.3-52 52-52 52 23.3 52 52-23.3 52-52 52z"></path></svg>
